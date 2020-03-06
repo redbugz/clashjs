69384ba7a946eefb24423857b25e30a902831932
Hamza Munaf <46735474+hamzamunaf@users.noreply.github.com>
2020-03-05 21:32:36 -0800

    Update ClashCore.test.js

--- src/clashjs/ClashCore.test.js
+++ src/clashjs/ClashCore.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import expect from 'expect'
 // import TestUtils from 'react-addons-test-utils'
+configure({ adapter: new Adapter() });
 
 import {ClashJS} from './ClashCore';
 jest.mock('./ClashCore');
@@ -12,10 +14,8 @@ jest.mock('./ClashCore');
 
 // Enzyme.configure({ adapter: new Adapter() })
 //
-it('should return the product', async () => {
-  const ClashTestClass =  new ClashJS();
-  const result = ClashTestClass.getState(); // Will throw error!
-  const CreateAmmo = ClashTestClass._createAmmo();
+it('Clash Core should return all the functions', async () => {
+  const ClashTestClass = new ClashJS();
   const expectedProduct = {
     "_createAmmo": '[Function $createAmmo]',
     "_getAlivePlayerCount": '[Function $getAlivePlayerCount]',
@@ -26,14 +26,22 @@ it('should return the product', async () => {
     "setupGame": '[Function setupGame]'
 
   };
-  console.log(ClashTestClass);
-  // console.log(CreateAmmo);
-  // expect(result).toBeTruthy();
-  expect(ClashTestClass).toBeTruthy();
+  // console.log(ClashTestClass);
+  expect(ClashTestClass).toBeTruthy(expectedProduct);
 });
 
-//
-// describe('ClashJS', () => {
-//     it('should exist', () => {
-//         expect(ClashJS).toExist()
-//     });
+
+import {PlayerClass} from './PlayerClass';
+jest.mock('./PlayerClass');
+
+it('Player Class should return all the functions', async () => {
+  const PlayerTestClass = new PlayerClass();
+  const expectedProduct = {
+    "getId": '[Function: getId]',
+    "getInfo()": '[Function $getInfo]',
+    "getName()": '[Function $getName]',
+    "playExplosion()": '[Function $playExplosion]'
+  };
+   // console.log(PlayerTestClass);
+  expect(PlayerTestClass).toBeTruthy(expectedProduct);
+});

a8724fb6431016b487283319b7012c7129ae1004
redbugz <allredlt@familysearch.org>
2020-04-24 01:37:22 -0600

    fix merge conflicts

--- src/players/horror.js
+++ src/players/horror.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var utils = require('../lib/utils.js');
 
 var MUSOLINI = {
@@ -23,29 +22,3 @@ var MUSOLINI = {
 };
 
 module.exports = MUSOLINI;
-=======
-var utils = require('../lib/utils.js');
-
-var MUSOLINI = {
-  info: {
-    name: 'Horror',
-    style: 10
-  },
-  ai: (playerState, enemiesStates, gameEnvironment) => {
-    var directionToAmmo;
-
-    if (utils.canKill(playerState, enemiesStates) && playerState.ammo) {
-      return 'shoot';
-    }
-    if (gameEnvironment.ammoPosition.length) {
-      directionToAmmo = utils.fastGetDirection(playerState.position, gameEnvironment.ammoPosition[0]);
-
-      if (directionToAmmo !== playerState.direction) return directionToAmmo;
-      return 'move';
-    }
-    return utils.safeRandomMove();
-  }
-};
-
-module.exports = MUSOLINI;
->>>>>>> cs362/master
--- src/players/xmontoya.js
+++ src/players/xmontoya.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var utils = require('../lib/utils.js');
 
 var xmontoya = {
@@ -25,31 +24,3 @@ var xmontoya = {
 };
 
 module.exports = xmontoya;
-=======
-var utils = require('../lib/utils.js');
-
-var xmontoya = {
-  info: {
-    name: 'Xmontoya89',
-    style: 1
-  },
-  ai: (playerState, enemiesStates, gameEnvironment) => {
-    var directionToAmmo;
-
-    if (utils.canKill(playerState, enemiesStates) && playerState.ammo) {
-      return 'shoot';
-    }
-
-    if (gameEnvironment.ammoPosition.length) {
-      directionToAmmo = utils.fastGetDirection(playerState.position, gameEnvironment.ammoPosition[0]);
-
-      if (directionToAmmo !== playerState.direction) return directionToAmmo;
-      return utils.safeRandomMove();
-    }
-
-    return 'move';
-  }
-};
-
-module.exports = xmontoya;
->>>>>>> cs362/master

0b18f3e49cf065f6e294dafd51af75a206636124
redbugz <allredlt@familysearch.org>
2020-05-05 21:06:28 -0600

    stats and bot clones

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,8 @@
 module.exports = {
   starterbot: require("./players/starterbot"),
+  starterbot2: require("./players/starterbot2"),
+  starterbot3: require("./players/starterbot3"),
+  starterbot4: require("./players/starterbot4"),
   random: require("./players/randombot"),
   // beasty: require("./players/beasty"),
 };
